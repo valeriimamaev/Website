e9dedd05e60ac26f391491a2038fce99443adecf
valeriimamaev <info.mav20@gmail.com>
2024-03-25 17:43:51 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".team-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+  
+      200: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      750: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1024: {
+        slidesPerView: 2.5,
+        spaceBetween: 30
+      },
+      1100: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+    }
+  });
+  
